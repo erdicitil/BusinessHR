da8d8246044ba35905e19a8389c7e0d3a36d7dbb
Ömer Özkan <omeerozkan52gmail.com>
2019-08-19 22:58:13 +0300

    Entityler oluşturuluyor.

---
+++ BusinessHR.Model/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessHR.Model
+{
+    public class Position:BaseEntity
+    {
+        public string Name { get; set; }
+        
+        public virtual ICollection<Department> Department { get; set; }
+    }
+}
