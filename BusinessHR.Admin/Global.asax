ee75f129400b7adf6395092a9b527633c45043b2
Erdi Çitil <erdicitil@hotmail.com>
2019-08-19 21:08:08 +0300

    Add project files.

---
+++ BusinessHR.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BusinessHR.Admin.MvcApplication" Language="C#" %>
---
+++ BusinessHR.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BusinessHR.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessHR.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(): base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
