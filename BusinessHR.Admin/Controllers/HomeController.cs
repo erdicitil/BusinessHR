e421229db1a0bbc0685a349f54a6f1db665a371b
erdicitil <erdicitil92@gmail.com>
2019-09-13 23:22:12 +0300

    commit

--- BusinessHR.Admin/Controllers/HomeController.cs
+++ BusinessHR.Admin/Controllers/HomeController.cs
@@ -1,31 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using AutoMapper;
+using BusinessHR.Admin.Models;
+using BusinessHR.Service;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace IdentitySample.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IEmployeeService employeeService;
+        private readonly IPermissionService permissionService;
+        public HomeController(IEmployeeService employeeService,IPermissionService permissionService)
+        {
+            this.employeeService = employeeService;
+            this.permissionService = permissionService;
+        }
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var employee = Mapper.Map<IEnumerable<EmployeeViewModel>>(employeeService.GetAll());
+            return View(employee);
         }
 
-        [HttpGet]
-        [Authorize]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
